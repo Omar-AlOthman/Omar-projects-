6b0a81d1004782befaac9e8f33e2b69cac4a80da
Omar-AlOthman <alothmanomar87@gmail.com>
2024-07-21 17:38:07 +0300

    Update and rename count down timer.py to count down timer minutes : seconds.py

--- count down timer minutes : seconds.py
+++ count down timer minutes : seconds.py
@@ -1,10 +1,13 @@
+
 import time 
 print ("Welcome to the count down timer.")
 def get_countdown_seconds():
     while True:
         try:
-            count_amount = int(input("Seconds of countdown = ")) 
-            return count_amount  # Return the valid input
+            time_input = input("Enter the countdown time (minutes:seconds) = ")
+            minutes, seconds = map(int,time_input.split(":"))
+            total_seconds = minutes * 60 + seconds 
+            return total_seconds  # Return the valid input
         except ValueError:
             print("Wrong input, please enter a valid integer in should be a number.")
 
@@ -12,10 +15,12 @@ def get_countdown_seconds():
 
 def countdown(seconds):
     while seconds > 0:
-        print(seconds)
+        minutes , sec = divmod(seconds,60)
+        time_str = f"{minutes:02}:{sec:02}"
+        print (time_str)
         time.sleep(1)  # Wait for one second
         seconds -= 1
     print("Time is up!!")
-
+    
 count_amount = get_countdown_seconds()
 countdown(count_amount)
